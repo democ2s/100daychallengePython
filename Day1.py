99b9720d67d454de56e7a36662cf26c80ccf9e5f
AzureSPO <30694979+democ2s@users.noreply.github.com>
2023-09-26 02:33:19 +0530

    Initial commit
    Day 1 - Working file

---
+++ Day1.py
@@ -0,0 +1,22 @@
+"""
+Project Title: 100 days of code: The complete Python Pro Bootcamp 2023, Dr.Angela Yu
+File Title: Day 1
+Date: 25/Sep/2023
+"""
+
+# Accepting input from users and displaying result to user
+
+print("Hello world.") # same as Console.WriteLine("");
+var_name = input("Enter your name") # Console.Read();
+print("Hello World \n") # Same as #9, inserts newline after displaying
+print ("What is your age "+ var_name) # string concatenation
+
+# variables
+var_str = "Apples" # No need of var / let keyword like JavaScript
+
+# string helper functions
+#len
+print(len("Apples")) # same as string.Length in C#
+
+
+
